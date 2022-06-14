f7795df87461867cadab0d1f5d3761887cfc4854
SwagBeota <micheleqrs@gmail.com>
2022-06-14 17:37:24 +0200

    Added dependencies, graphql schemas and classes

--- src/main/java/com/examproject/ordermanager/OrderManagerApplication.java
+++ src/main/java/com/examproject/ordermanager/OrderManagerApplication.java
@@ -3,11 +3,27 @@ package com.examproject.ordermanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrderManagerApplication {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@SpringBootApplication
+public class OrderManagerApplication implements CommandLineRunner {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String sql = "INSERT INTO agents VALUES ('Prova', 'AAAAA', 'AAAA', '402', '000-432432', '')";
+
+		int rows = jdbcTemplate.update(sql);
+		if (rows > 0) {
+			System.out.println("A new row has been inserted.");
+		}
 	}
 
 }
---
+++ src/main/java/com/examproject/ordermanager/model/Agent.java
@@ -0,0 +1,9 @@
+package com.examproject.ordermanager.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Agent {
+    @Id
+
+}
